9f3e6f16d956bfe3c2a26e67cf36fa0b46923361
Gabriel Genê <gabrielgene97@gmail.com>
2018-06-24 19:36:32 -0300

    Add new routes

--- src/index.js
+++ src/index.js
@@ -1,22 +1,28 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import { BrowserRouter as Router, Route, Redirect, Switch } from 'react-router-dom';
+import { MuiThemeProvider } from '@material-ui/core/styles';
+
 import Login from './pages/login';
+import AlertsList from './pages/alerts-list';
+import NewAlert from './pages/new-alert';
+
+import { theme } from './theme';
 import './index.css';
 import registerServiceWorker from './registerServiceWorker';
 
-const Home = () => <h1>Home</h1>
-
 const App = () => (
-  <Router>
-    <Switch>
-      <Route exact path="/" component={Login} />
-      <Route path="/home" component={Home} />
-      <Redirect to="/home" />
-    </Switch>
-  </Router>
-
-)
+  <MuiThemeProvider theme={theme}>
+    <Router>
+      <Switch>
+        <Route exact path="/" component={Login} />
+        <Route path="/avisos" component={AlertsList} />
+        <Route path="/novo-aviso" component={NewAlert} />
+        <Redirect to="/avisos" />
+      </Switch>
+    </Router>
+  </MuiThemeProvider>
+);
 
 ReactDOM.render(<App />, document.getElementById('root'));
 registerServiceWorker();
