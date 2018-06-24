5008f25ff06ead9c9f843f50654a1fbd4fdd2c2b
Gabriel Genê <gabrielgene97@gmail.com>
2018-06-24 19:37:02 -0300

    Configure theme

---
+++ src/theme.js
@@ -0,0 +1,18 @@
+import { createMuiTheme } from '@material-ui/core/styles';
+
+export const theme = createMuiTheme({
+  palette: {
+    primary: {
+      light: '#1B69B0',
+      main: '#063C7F',
+      dark: '#000000',
+      contrastText: '#fff',
+    },
+    secondary: {
+      light: '#F3E466',
+      main: '#EED929',
+      dark: '#EBAF20',
+      contrastText: '#000',
+    },
+  },
+});
