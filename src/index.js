8e3920ba79dac23e7b77d2339543f845a59d8e13
Gabriel Genê <gabrielgene97@gmail.com>
2018-06-24 17:38:23 -0300

    Implement routes

--- src/index.js
+++ src/index.js
@@ -1,8 +1,22 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { BrowserRouter as Router, Route, Redirect, Switch } from 'react-router-dom';
+import Login from './pages/login';
 import './index.css';
-import App from './App';
 import registerServiceWorker from './registerServiceWorker';
+
+const Home = () => <h1>Home</h1>
+
+const App = () => (
+  <Router>
+    <Switch>
+      <Route exact path="/" component={Login} />
+      <Route path="/home" component={Home} />
+      <Redirect to="/home" />
+    </Switch>
+  </Router>
+
+)
 
 ReactDOM.render(<App />, document.getElementById('root'));
 registerServiceWorker();
