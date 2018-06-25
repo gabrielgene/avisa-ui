8705a90c6cbcfb1d11d80f9e7471b7b3b7573f20
Gabriel Genê <gabrielgene97@gmail.com>
2018-06-25 01:26:36 -0300

    Improve alerts list

--- src/pages/alerts-list/index.jsx
+++ src/pages/alerts-list/index.jsx
@@ -4,40 +4,64 @@ import Button from '@material-ui/core/Button';
 import { withStyles } from '@material-ui/core/styles';
 import Topbar from '../../components/topbar';
 
-const styles = {
+const styles = theme => ({
   buttonWrapper: {
     position: 'fixed',
-    bottom: 16,
+    bottom: theme.spacing.unit * 2,
     width: '100%',
     display: 'flex',
     justifyContent: 'center',
   },
+  button: {
+    borderRadius: 10,
+  },
   empty: {
-    marginTop: 108,
+    marginTop: theme.spacing.unit * 13.5,
     textAlign: 'center',
   },
   text: {
-    marginTop: 8,
+    marginTop: theme.spacing.unit * 2,
     fontFamily: 'Raleway',
-    fontSize: 16,
+    fontSize: 18,
     fontWeight: 600,
     color: 'rgb(0, 0, 0, 0.54)',
     marginRight: 24,
     marginLeft: 24,
   },
-};
+  card: {
+    border: '1px solid rgba(0, 0, 0, 0.12)',
+    margin: theme.spacing.unit * 3,
+    padding: theme.spacing.unit * 2,
+    borderRadius: theme.spacing.unit * 3,
+    backgroundColor: 'rgba(255, 255, 255, 1)',
+  },
+  cardText: {
+    fontFamily: 'Raleway',
+    fontSize: 20,
+    fontWeight: 600,
+    color: 'rgb(0, 0, 0, 0.87)',
+  },
+});
 
 const AlertsList = ({ classes, history }) => (
   <div>
     <Topbar title="SEUS AVISOS" />
-    <div className={classes.empty}>
-      <img src="https://cdn.sympli.io/bundles/5b300eade45cec2b7d1188a4/44542ec64b4009f966c07622bbe9917f33960d7387/bundle/web%2FGroup_6-2x.png" alt="avisos" />
-      <div className={classes.text}>
-        Crie um alerta e seja notificado quando um novo produto for publicado de acordo com suas configurações
-      </div>
-    </div>
+    {
+      !!history.location.search ?
+        <div className={classes.card}>
+          <div className={classes.cardText}>smartphone 128gb</div>
+        </div>
+        :
+        <div className={classes.empty}>
+          <img src="https://cdn.sympli.io/bundles/5b300eade45cec2b7d1188a4/44542ec64b4009f966c07622bbe9917f33960d7387/bundle/web%2FGroup_6-2x.png" alt="avisos" />
+          <div className={classes.text}>
+            Crie um alerta e seja notificado quando um novo produto for publicado de acordo com suas configurações
+          </div>
+        </div>
+    }
     <div className={classes.buttonWrapper}>
       <Button
+        className={classes.button}
         variant="contained"
         color="primary"
         onClick={() => history.push('/novo-aviso')}
