e9dee18611b265d9c45f723115d41c472188d440
Gabriel Genê <gabrielgene97@gmail.com>
2018-06-25 01:26:27 -0300

    Improve topbar

--- src/components/topbar/index.jsx
+++ src/components/topbar/index.jsx
@@ -4,12 +4,19 @@ import Toolbar from '@material-ui/core/Toolbar';
 import Typography from '@material-ui/core/Typography';
 import IconButton from '@material-ui/core/IconButton';
 import MenuIcon from '@material-ui/icons/Menu';
+import ArrowBackIcon from '@material-ui/icons/ArrowBack';
+import { withRouter } from 'react-router-dom';
 
-const Topbar = ({ title }) => (
+const Topbar = ({ title, back, history }) => (
   <AppBar position="static">
     <Toolbar>
       <IconButton color="inherit" aria-label="Menu">
-        <MenuIcon />
+        {
+          back ?
+            <ArrowBackIcon onClick={history.goBack}/>
+            :
+            <MenuIcon />
+        }
       </IconButton>
       <Typography variant="title" color="inherit">
         {title}
@@ -18,4 +25,4 @@ const Topbar = ({ title }) => (
   </AppBar>
 );
 
-export default Topbar;
+export default withRouter(Topbar);
