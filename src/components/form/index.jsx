5497176929b893746e391ed6bb2713ef0f57a223
Gabriel Genê <gabrielgene97@gmail.com>
2018-06-27 01:10:53 -0300

    Remove checkbox

--- src/components/form/index.jsx
+++
@@ -1,9 +0,0 @@
-import React from 'react';
-
-import Checkbox from './checkbox';
-import Select from './select';
-
-export default class Form extends React.Component {
-  static Checkbox = Checkbox;
-  static Select = Select;
-}
